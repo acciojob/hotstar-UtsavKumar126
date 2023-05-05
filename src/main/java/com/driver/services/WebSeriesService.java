71f7afe99f3f27ca9192fc1efe787c68890e98ba
utsav <guptautsavkumar126@gmail.com>
2023-05-05 13:54:35 +0530

    add_webseries

--- src/main/java/com/driver/services/WebSeriesService.java
+++ src/main/java/com/driver/services/WebSeriesService.java
@@ -17,33 +17,35 @@ public class WebSeriesService {
     @Autowired
     ProductionHouseRepository productionHouseRepository;
 
-    public Integer addWebSeries(WebSeriesEntryDto webSeriesEntryDto)throws  Exception{
+    public Integer addWebSeries(WebSeriesEntryDto webSeriesEntryDto)throws  Exception {
 
         //Add a webSeries to the database and update the ratings of the productionHouse
-        ProductionHouse productionHouse=productionHouseRepository.findById(webSeriesEntryDto.getProductionHouseId()).get();
+        ProductionHouse productionHouse = productionHouseRepository.findById(webSeriesEntryDto.getProductionHouseId()).get();
         //Incase the seriesName is already present in the Db throw Exception("Series is already present")
-        WebSeries webSeries=webSeriesRepository.findBySeriesName(webSeriesEntryDto.getSeriesName());
-        if(webSeries!=null)throw new Exception("Series is already present");
+        if (webSeriesRepository.findBySeriesName(webSeriesEntryDto.getSeriesName()) != null) {
+            throw new Exception("Series is already present");
+        }
         //use function written in Repository Layer for the same
-        //Dont forget to save the production and webseries Repo
-        WebSeries webSeries1=new WebSeries();
-        webSeries1.setSeriesName(webSeriesEntryDto.getSeriesName());
-        webSeries1.setAgeLimit(webSeriesEntryDto.getAgeLimit());
-        webSeries1.setRating(webSeriesEntryDto.getRating());
-        webSeries1.setSubscriptionType(webSeriesEntryDto.getSubscriptionType());
-        webSeries1.setProductionHouse(productionHouse);
-        productionHouse.getWebSeriesList().add(webSeries1);
+        else {//Dont forget to save the production and webseries Repo
+            WebSeries webSeries1 = new WebSeries();
+            webSeries1.setSeriesName(webSeriesEntryDto.getSeriesName());
+            webSeries1.setAgeLimit(webSeriesEntryDto.getAgeLimit());
+            webSeries1.setRating(webSeriesEntryDto.getRating());
+            webSeries1.setSubscriptionType(webSeriesEntryDto.getSubscriptionType());
+            webSeries1.setProductionHouse(productionHouse);
+            productionHouse.getWebSeriesList().add(webSeries1);
 
-        double curRating=productionHouse.getRatings();
-        int noOfwebSerires=productionHouse.getWebSeriesList().size();
+            double curRating = productionHouse.getRatings();
+            int noOfwebSerires = productionHouse.getWebSeriesList().size();
 
-        double newRating=((curRating*noOfwebSerires)+webSeriesEntryDto.getRating())/noOfwebSerires+1;
+            double newRating = ((curRating * noOfwebSerires) + webSeriesEntryDto.getRating()) / noOfwebSerires + 1;
 
-        productionHouse.setRatings(newRating);
+            productionHouse.setRatings(newRating);
 
-        productionHouseRepository.save(productionHouse);
+            productionHouseRepository.save(productionHouse);
 
-        return webSeriesRepository.findBySeriesName(webSeriesEntryDto.getSeriesName()).getId();
+            return webSeries1.getId();
+        }
     }
 
 }
